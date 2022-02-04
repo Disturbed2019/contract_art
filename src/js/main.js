ac3cfca03961b2341967c723059b3cb5e9300e24
dallas <disturbe@bk.ru>
2022-02-04 16:23:58 +0300

    day2

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
+import Swiper from './swiper-bundle';
 
-
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 5,
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
