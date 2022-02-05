287b70a9e7930920a28096e83d4572b4a13176a0
Corban_Dallas <disturbe@bk.ru>
2022-02-05 13:46:53 +0300

    Desktop resolution ready

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,9 @@ import Swiper from './swiper-bundle';
 const swiper = new Swiper(".swiper", {
     slidesPerView: 5,
     loop:true,
+    autoplay: {
+        delay: 2000,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
